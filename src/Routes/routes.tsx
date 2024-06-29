a64a58061c532c890b54451b8f7205d464102494
sajib9090 <nispap.devil.sajib@gmail.com>
2024-06-29 09:56:24 +0600

    google login done

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -2,19 +2,24 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Register from "../pages/Register/Register";
 import Login from "../pages/Login/Login";
+import GoogleLoginCallback from "../pages/GoogleLoginCallback/GoogleLoginCallback";
 
 const routes = createBrowserRouter([
-    {
-        path:"/",
-        element:<App />,
-    },
-    {
-        path:"/login",
-        element:<Login />,
-    },
-    {
-        path:"/register",
-        element:<Register />,
-    }
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/login/success",
+    element: <GoogleLoginCallback />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
 ]);
-export default routes
+export default routes;
