e947693d8eec9e258f0d1d90070871d30131d127
Mominul <ulhouqemomin39@gmail.com>
2024-06-30 11:10:34 +0600

    added eyes

--- src/pages/Home/PostCard.tsx
+++ src/pages/Home/PostCard.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { Card } from "antd";
-import { LikeOutlined, LikeFilled, CommentOutlined } from "@ant-design/icons";
+import { LikeOutlined, LikeFilled, CommentOutlined ,EyeFilled} from "@ant-design/icons";
 import PostActionsMenu from "../../components/PostActionsMenu";
+
 import {
   useAddLikeCommentMutation,
   useGetAllPostsQuery,
@@ -11,11 +12,12 @@ import { useAppSelector } from "../../hooks/hooks";
 import { useUserInfo } from "../../redux/features/auth/authSlice";
 import CommentsSection from "../../components/CommentsSection ";
 
+
+
 interface Comment {
   id: number;
   text: string;
   author: string;
-  postId: string;
 }
 
 interface Post {
@@ -31,11 +33,11 @@ interface Post {
 const PostCard: React.FC = () => {
   const userInfo = useAppSelector(useUserInfo);
   const [comments, setComments] = useState<Comment[]>([]);
-  const [showComments, setShowComments] = useState<string | null>(null);
 
-  const { data: posts } = useGetAllPostsQuery({});
+  const { data: posts, } = useGetAllPostsQuery({});
   const [addLikeComment, { isLoading: addLikeLoading }] =
     useAddLikeCommentMutation();
+
 
   const handleLike = async (postId: string) => {
     const data = {
@@ -49,85 +51,71 @@ const PostCard: React.FC = () => {
     }
   };
 
-  const addComment = (comment: Omit<Comment, "postId">, postId: string) => {
-    setComments((prevComments) => [...prevComments, { ...comment, postId }]);
-  };
-
-  const toggleComments = (postId: string) => {
-    setShowComments((prev) => (prev === postId ? null : postId));
+  const addComment = (comment: Comment) => {
+    setComments([...comments, comment]);
   };
 
   return (
     <div className="mt-6 px-1">
       {posts?.data?.map((post: Post) => (
-        <div key={post._id} className="border p-4 my-3 rounded-lg shadow">
-          <Card
-            className="my-2 border-none"
-            actions={[
-              <span onClick={() => handleLike(post.post_id)} key="like">
-                {post?.post_additional?.likes?.some(
-                  (like: { user_id: string | undefined }) =>
-                    like.user_id === userInfo?.user_id
-                ) ? (
-                  <LikeFilled
-                    disabled={addLikeLoading}
-                    className="text-blue-500"
-                  />
-                ) : (
-                  <LikeOutlined disabled={addLikeLoading} />
-                )}{" "}
-                {post?.post_additional?.likes?.length}
-              </span>,
-              <span
-                key="comment"
-                onClick={() => toggleComments(post.post_id)}
-                className="cursor-pointer"
-              >
-                <CommentOutlined />
-              </span>,
-              <PostActionsMenu key="actions" postId={post.post_id} />,
-            ]}
-          >
-            <div className="flex items-center mb-4">
+        <div  className="border p-4 my-3 rounded-lg shadow">
+        <Card
+          key={post?._id}
+          className="  border-none"
+          actions={[
+            <span onClick={() => handleLike(post?.post_id)} key="like">
+              {post?.post_additional?.likes?.some(
+                (like: { user_id: string | undefined }) => like?.user_id === userInfo?.user_id
+              ) ? (
+                <LikeFilled
+                  disabled={addLikeLoading}
+                  className="text-blue-500"
+                />
+              ) : (
+                <LikeOutlined disabled={addLikeLoading} />
+              )}{" "}
+              {post?.post_additional?.likes?.length}
+            </span>,
+
+            <CommentOutlined key="comment" />,
+
+            <PostActionsMenu key="actions" postId={post?.post_id} />,
+          ]}
+        >
+          <div className="flex items-center mb-4">
+            <img
+              className="w-10 h-10 border-2 border-blue-500 rounded-full"
+              src={
+                post?.user_info?.avatar?.url ||
+                "https://api.dicebear.com/7.x/miniavs/svg?seed=8"
+              }
+              alt=""
+            />
+            <div className="ml-4">
+              <h4 className="font-medium text-lg capitalize">
+                {post?.user_info?.name}
+              </h4>
+              <small className="text-gray-500">
+                {format(new Date(post?.createdAt), "MMMM do, yy h:mm a")}
+              </small>
+            </div>
+          </div>
+          <div className="mb-2">
+            {post?.post_image?.url && (
               <img
-                className="w-10 h-10 border-2 border-blue-500 rounded-full"
-                src={
-                  post?.user_info?.avatar?.url ||
-                  "https://api.dicebear.com/7.x/miniavs/svg?seed=8"
-                }
+                className="w-full my-2 rounded-lg"
+                src={post?.post_image?.url}
                 alt=""
               />
-              <div className="ml-4">
-                <h4 className="font-medium text-lg capitalize">
-                  {post?.user_info?.name}
-                </h4>
-                <small className="text-gray-500">
-                  {format(new Date(post.createdAt), "MMMM do, yy h:mm a")}
-                </small>
-              </div>
+            )}
+            <p className="text-gray-700">{post?.post_description}</p>
+          </div>
+            <div className="flex items-center justify-start">
+            <EyeFilled className="text-xl"></EyeFilled><span className="ml-2">(65)</span>
             </div>
-            <div className="mb-3">
-              {post?.post_image?.url && (
-                <img
-                  className="w-full my-2 rounded-lg"
-                  src={post.post_image.url}
-                  alt=""
-                />
-              )}
-              <p className="text-gray-700">{post.post_description}</p>
-            </div>
-          </Card>
-
-          {showComments === post.post_id && (
-            <CommentsSection
-              comments={comments.filter(
-                (comment) => comment.postId === post.post_id
-              )}
-              addComment={(comment: Omit<Comment, "postId">) =>
-                addComment(comment, post.post_id)
-              }
-            />
-          )}
+        </Card>
+    
+          <CommentsSection comments={comments} addComment={addComment} />
         </div>
       ))}
     </div>
